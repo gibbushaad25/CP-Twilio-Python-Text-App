9c882d3842ecfb87af9d5863b078591a26b847c9
gibbushaad25 <33212215+gibbushaad25@users.noreply.github.com>
2017-10-30 04:01:59 +0000

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+account_sid = "ACeaf9b10ac19b1d751cd154b679e79714"
+auth_token = "4de58b3df3d7eaab31e62e4dbafb1d39"
+my_cell = "+353899583220"
+my_twilio = "+353861802307"
